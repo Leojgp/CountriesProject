cdbc4613acb3331e43f6d1383e2cac6190d82f6c
Leojgp <leonardoljgp@gmail.com>
2024-12-10 18:01:30 +0100

    Pantalla Principal hecha y Redux tool-kits instalado

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Home'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
